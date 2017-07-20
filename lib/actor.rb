87e86d6b46fc1eb3465c1465fd73ba97db109bd6
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-20 12:20:24 +0300

    this commit updates the update methos and adds a movie method

--- lib/actor.rb
+++ lib/actor.rb
@@ -35,9 +35,24 @@ class Actor
 
     define_method(:update) do |attributes|
       @name = attributes.fetch(:name, @name)
-      @id = self.id()
-      DB.exec("UPDATE actors SET name = '#{@name}' WHERE id = #{@id};")
+      DB.exec("UPDATE actors SET name = '#{@name}' WHERE id = #{self.id()};")
+
+      attributes.fetch(:movie_ids, []).each() do |movie_id|
+        DB.exec("INSERT INTO actors_movies (actor_id, movie_id) VALUES (#{self.id()}, #{movie_id});")
+      end
     end
+    define_method(:movies) do
+    actor_movies = []
+    results = DB.exec("SELECT movie_id FROM actors_movies WHERE actor_id = #{self.id()};")
+    results.each() do |result|
+      movie_id = result.fetch("movie_id").to_i()
+      movie = DB.exec("SELECT * FROM movies WHERE id = #{movie_id};")
+      name = movie.first().fetch("name")
+      actor_movies.push(Movie.new({:name => name, :id => movie_id}))
+    end
+    actor_movies
+  end
+
 
     define_method(:delete) do
       DB.exec("DELETE FROM actors WHERE id = #{self.id()};")
