b90a557a668464f05e5227351d89c4885c5be6eb
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-20 12:15:22 +0300

    this commit adds the actor method, to display the actors in a movie

--- lib/movie.rb
+++ lib/movie.rb
@@ -40,6 +40,17 @@ class Movie
         DB.exec("INSERT INTO actors_movies (actor_id, movie_id) VALUES (#{actor_id}, #{self.id()});")
       end
     end
+    define_method(:actors) do
+    movie_actors = []
+    results = DB.exec("SELECT actor_id FROM actors_movies WHERE movie_id = #{self.id()};")
+    results.each() do |result|
+      actor_id = result.fetch("actor_id").to_i()
+      actor = DB.exec("SELECT * FROM actors WHERE id = #{actor_id};")
+      name = actor.first().fetch("name")
+      movie_actors.push(Actor.new({:name => name, :id => actor_id}))
+    end
+    movie_actors
+  end
 
     define_method(:delete) do
       DB.exec("DELETE FROM movies WHERE id = #{self.id()};")
