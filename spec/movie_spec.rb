d606c59fc28f6ce1b99f3c17c80c62f67dd98914
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-20 11:57:30 +0300

    this commit will add a rezt for the actor method

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -55,4 +55,16 @@ require("spec_helper")
         expect(Movie.all()).to(eq([movie2]))
       end
     end
+    describe("#actors") do
+    it("returns all of the actors in a particular movie") do
+      movie = Movie.new({:name => "Oceans Eleven", :id => nil})
+      movie.save()
+      george = Actor.new({:name => "George Clooney", :id => nil})
+      george.save()
+      brad = Actor.new({:name => "Brad Pitt", :id => nil})
+      brad.save()
+      movie.update({:actor_ids => [george.id(), brad.id()]})
+      expect(movie.actors()).to(eq([george, brad]))
+    end
+  end
   end
